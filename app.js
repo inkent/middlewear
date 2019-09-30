842ecaff3a040a56507b719d3e595248f3378896
Dave be <dave@inkentertainment.com>
2019-09-30 18:19:24 -0400

    now with more...working

--- app.js
+++ app.js
@@ -5,31 +5,19 @@ const axios = require('axios');
 const app = express()
 const port = 3000
 
-var fs = require('fs');
-var util = require('util');
-var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
-var log_stdout = process.stdout;
+// var fs = require('fs');
+// var util = require('util');
+// var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
+// var log_stdout = process.stdout;
 
-console.log = function(d) { //
-  log_file.write(util.format(d) + '\n');
-  log_stdout.write(util.format(d) + '\n');
-};
+// console.log = function(d) { //
+//   log_file.write(util.format(d) + '\n');
+//   log_stdout.write(util.format(d) + '\n');
+// };
 
 var allowedOrigins = ['http://localhost:3000','http://localhost:8000',
                       'https://solarismusicfestival.com','http://solarismusicfestival.com','http://winkcannabis.com','https://winkcannabis.com','https://sofiayorkville.com'];
-app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 
 
 app.use(bodyParser.json())
@@ -41,10 +29,10 @@ app.get('/', function (req, res) {
 })
 
 app.post('/mid', function (req, res) {
-console.log(req.body)
+
 	if(req.body){
-const response = req.body 
-	const payload = {
+ 
+	let thepayload = {
     'api_key': req.body.key,
     'email_address': req.body.email,
     'fields': {
@@ -53,16 +41,17 @@ const response = req.body
     },
     'status': 'SUBSCRIBED'
 }
+ 
+const theurl = req.body.url.toString()
 
 
-
-axios.post("https://emailoctopus.com/api/1.5/lists/0d1a3c35-d003-11e9-9307-06b4694bee2a/contacts",payload)
+axios.post(theurl,thepayload)
 .then(e => {
-  console.log("message",e)
+console.log("success",e)
 	res.status(200).send('Thank You For Subscribing')
 })
 .catch(err => {
-	console.log("responded with",err)
+console.log("error",err)
 	res.status(400).send('Something is Wrong With The Email')
 })
 
@@ -74,15 +63,15 @@ axios.post("https://emailoctopus.com/api/1.5/lists/0d1a3c35-d003-11e9-9307-06b46
 
 })
 
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
+// if (app.get('env') === 'development') {
+//   app.use(function(err, req, res, next) {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//       message: err.message,
+//       error: err
+//     });
+//   });
+// }
 
 module.exports = app;
 
